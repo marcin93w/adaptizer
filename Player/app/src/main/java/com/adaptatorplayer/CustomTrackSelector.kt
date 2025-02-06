ae3051821b7e4f4b7c954cc78c18b410b6eb7bee
marcin93w <marcin93w@gmail.com>
2025-02-06 22:34:58 +0100

    Adaptator Player Android App

---
+++ Player/app/src/main/java/com/adaptatorplayer/CustomTrackSelector.kt
@@ -0,0 +1,38 @@
+package com.adaptatorplayer
+
+import androidx.media3.common.C
+import androidx.media3.common.Timeline
+import androidx.media3.common.util.NullableType
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.RendererConfiguration
+import androidx.media3.exoplayer.source.MediaSource
+import androidx.media3.exoplayer.trackselection.ExoTrackSelection
+import androidx.media3.exoplayer.trackselection.FixedTrackSelection
+import androidx.media3.exoplayer.trackselection.MappingTrackSelector
+
+@UnstableApi
+class CustomTrackSelector() : MappingTrackSelector() {
+
+    override fun selectTracks(
+        mappedTrackInfo: MappingTrackSelector.MappedTrackInfo,
+        rendererFormatSupports: Array<out Array<out IntArray>>,
+        rendererMixedMimeTypeAdaptationSupport: IntArray,
+        mediaPeriodId: MediaSource.MediaPeriodId,
+        timeline: Timeline
+    ): android.util.Pair<Array<out @NullableType RendererConfiguration?>, Array<out @NullableType ExoTrackSelection?>> {
+        val rendererConfiguration = arrayOfNulls<RendererConfiguration>(mappedTrackInfo.rendererCount)
+        val trackSelections = arrayOfNulls<ExoTrackSelection>(mappedTrackInfo.rendererCount)
+
+        for (i in 0 until mappedTrackInfo.rendererCount) {
+            val isAudio = mappedTrackInfo.getRendererType(i) == C.TRACK_TYPE_AUDIO
+
+            val trackGroupArray = mappedTrackInfo.getTrackGroups(i)
+            if (trackGroupArray.length > 0) {
+                trackSelections[i] = FixedTrackSelection(trackGroupArray.get(0), 0)
+                rendererConfiguration[i] = RendererConfiguration.DEFAULT
+            }
+        }
+
+        return android.util.Pair(rendererConfiguration, trackSelections)
+    }
+}
---
+++ Player/app/src/main/java/com/adaptatorplayer/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.adaptatorplayer
+
+import android.content.ContentResolver
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.annotation.OptIn
+import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.ui.PlayerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var exoPlayer: ExoPlayer
+    private lateinit var playerView: PlayerView
+
+    @OptIn(UnstableApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        playerView = findViewById(R.id.playerView)
+        val btnPlay = findViewById<Button>(R.id.btnPlay)
+        val btnStop = findViewById<Button>(R.id.btnStop)
+
+        val trackSelector = CustomTrackSelector()
+        exoPlayer = ExoPlayer.Builder(this)
+            .setTrackSelector(trackSelector)
+            .build()
+
+        playerView.player = exoPlayer
+
+        val rawResourceId = R.raw.output
+        val rawUri = Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE).path(
+            rawResourceId.toString()
+        ).build()
+        val mediaItem = MediaItem.fromUri(rawUri)
+
+        val dataSourceFactory = DefaultDataSource.Factory(this)
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(mediaItem)
+
+        exoPlayer.setMediaSource(mediaSource)
+        exoPlayer.prepare()
+
+        btnPlay.setOnClickListener {
+            exoPlayer.play()
+        }
+
+        btnStop.setOnClickListener {
+            exoPlayer.pause()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        exoPlayer.release()
+    }
+}
